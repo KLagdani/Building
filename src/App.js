bcc3a8ef6c081ff0bec414a69b1636409190b3d7
Kaoutar <lagdani.kaoutar@gmail.com>
2020-03-09 14:51:54 +0100

    ADD Revenues example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Container from "./components/Container";
+import Revenues from "./components/Revenues";
 
 function App() {
   return (
     <div className="App">
-      <Container />
+      <Revenues />
     </div>
   );
 }
